444e6b5c9af92be4286c70692671b4e3e8dadd2d
OVIS <ovi@ovswebsites.com>
2024-01-10 05:10:50 +0200

    fix: removed missing import

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -5,7 +5,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/devlink/global.css";
 import { DevLinkProvider } from "@/devlink";
-import LanguageChanger from "@/components/LanguageChanger";
 import { Header } from "@/components/Header";
 
 // i18n stuff
